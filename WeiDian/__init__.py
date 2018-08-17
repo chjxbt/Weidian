97f141bb5ba055354b954b2a4a275c10995c4b99
wktadmin <musicvae@gmail.com>
2018-08-17 16:59:54 +0800

    转小写

--- WeiDian/__init__.py
+++ WeiDian/__init__.py
@@ -9,15 +9,12 @@ class JSONEncoder(_JSONEncoder):
     """重写对象序列化, 当默认jsonify无法序列化对象的时候将调用这里的default"""
     def default(self, o):
         if hasattr(o, 'keys') and hasattr(o, '__getitem__'):
-            try:
-                res = dict(o)
-                for k in res.keys():
+            res = dict(o)
+            for k in res.keys():
+                if k[0].isupper():
                     res[k.lower()] = res[k]
                     res.pop(k)
-                return res
-            except:
-                import ipdb
-                ipdb.set_trace()
+            return res
         if isinstance(o, date):
             # 也可以序列化时间类型的对象
             return o.strftime('%Y-%m-%d')
