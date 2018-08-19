ac2678a3143290a83567991f811ff59e4f973027
wktadmin <musicvae@gmail.com>
2018-08-19 18:16:04 +0800

    测试token

--- WeiDian/apis/v1/test_maketoken.py
+++ WeiDian/apis/v1/test_maketoken.py
@@ -2,21 +2,17 @@ from flask import Blueprint
 from flask.views import MethodView
 
 from WeiDian.common.MakeToken import verify_token_decorator
+from test.errors import ApiException
 
 
 class TestToken(MethodView):
     def post(self):
         usid = '6882ad09-bf5f-4607-8ad1-1cd46b6158e0'
         from WeiDian.common.MakeToken import usid_to_token
-        return usid_to_token(usid)
-        return str(len(usid_to_token(usid)))
-
+        return usid_to_token(usid, model='SuperUser')
 
     @verify_token_decorator
     def get(self):
         from flask import request
-        request.user
-        import ipdb
-        ipdb.set_trace()
-
-        return ''
+        raise ApiException
+        return 'fsfd'
