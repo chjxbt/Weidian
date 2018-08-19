e109e823c1bcf20c718a05bc2918efa1b98a1bfb
wktadmin <musicvae@gmail.com>
2018-08-19 15:11:57 +0800

    基本设置

--- WeiDian/control/BaseControl.py
+++ WeiDian/control/BaseControl.py
@@ -16,7 +16,14 @@ class BaseControl():
         act.add('suuser', 'media', 'tags', 'foward', 'likenum', 'soldnum')
         return act
 
+    def fill_suser(self, obj):
+        """给对象添加一个用户字段"""
+        obj.suuser = self.suser.get_user_by_user_id(obj.USid)  # 对象的用户
+        obj.add('user')
+        return obj
+
     def fill_comment(self, act):
+        """给活动对象附加一个评论属性"""
         acid = act.ACid
         act.comment = self.sacomment.get_comment_by_activity_id(acid)
         act.add('comment')
@@ -24,7 +31,7 @@ class BaseControl():
         return act
 
     def fill_comment_apply_for(self, comment):
-        """"如果既是评论又是回复则添加一个'所回复用户'字段"""
+        """"如果既是评论又是回复则添加一个'所回复用户'属性"""
         acoid = comment.ACOid
         if not comment.ACOparentid:
             return comment  # 如果ACOid没有值, 说明这不是回复的内容
