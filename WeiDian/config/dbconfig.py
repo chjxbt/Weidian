4681549ac1ba849389a5a0bb5f856c1c1c5c29d8
wktadmin <musicvae@gmail.com>
2018-08-20 09:22:26 +0800

    加入环境变量

--- WeiDian/config/dbconfig.py
+++ WeiDian/config/dbconfig.py
@@ -1,7 +1,8 @@
+import os
 database = "weidian"
 host = "127.0.0.1"
 port = "3306"
-username = "username"
-password = "password"
+username = os.environ.get('DB_USER', 'root')
+password = os.environ.get('DB_PWD', '')
 charset = "utf8"
 sqlenginename = 'mysql+pymysql'
