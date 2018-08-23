0075ddf80f22f2faf88bd61b05dbd1580ae04a08
wktadmin <musicvae@gmail.com>
2018-08-23 22:54:51 +0800

    用户密码验证

--- WeiDian/service/SUser.py
+++ WeiDian/service/SUser.py
@@ -2,6 +2,8 @@
 import sys
 import os
 
+from werkzeug.security import check_password_hash
+
 sys.path.append(os.path.dirname(os.getcwd()))
 from SBase import SBase, close_session
 from WeiDian.models.model import User, Activity
@@ -24,3 +26,11 @@ class SUser(SBase):
     def get_user_by_user_id(self, usid):
         """通过活动id获取发布者"""
         return self.session.query(User).filter_by(USid=usid).first()
+
+    @close_session
+    def verify_super(self, usname, uspassword):
+        """通过用户名和密码验证"""
+        user = self.session.query(User).filter_by(USname=usname).first()
+        if user:
+            if check_password_hash(user.USpassword, uspassword):
+                return user
