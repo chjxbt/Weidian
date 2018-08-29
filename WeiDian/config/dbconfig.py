e3d5059b9fb7c191eea297dd7e1fb6c506960ce0
wktadmin <musicvae@gmail.com>
2018-08-29 18:19:51 +0800

    数据库设置

--- WeiDian/config/dbconfig.py
+++ WeiDian/config/dbconfig.py
@@ -1,6 +1,6 @@
 import os
-database = "weidian"
-host = "127.0.0.1"
+database = os.environ.get('DB_NAME', "weidian")
+host = os.environ.get('DB_HOST', "127.0.0.1")
 port = "3306"
 username = os.environ.get('DB_USER', 'root')
 password = os.environ.get('DB_PWD', '')
