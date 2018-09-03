c80ef03bea1542f0d6741fa25870fbc27c0a8fb1
maoyaY <1335679005@qq.com>
2018-09-03 17:01:56 +0800

    初始化

--- WeiDian/config/dbconfig.py
+++
@@ -1,8 +0,0 @@
-import os
-database = os.environ.get('DB_NAME', "weidian")
-host = os.environ.get('DB_HOST', "127.0.0.1")
-port = "3306"
-username = os.environ.get('DB_USER', 'root')
-password = os.environ.get('DB_PWD', '')
-charset = "utf8"
-sqlenginename = 'mysql+pymysql'
--- WeiDian/control/__init__.py
+++
@@ -1,4 +0,0 @@
-from .CActivity import CActivity
-from .CBanner import CBanner
-from .CHotMessage import CHotMessage
-from .CTopNav import CTopNav
