46cd59a67c9cafb956309e8d02dbcba6076cad73
Liuxy <liuxy0551@qq.com>
2018-09-05 09:45:30 +0800

    新api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 
 // const title = 'http://10.0.0.63:7443';
-const title = 'http://120.79.182.43:7443';
+// const title = 'http://120.79.182.43:7443';
+const title = 'https://daaiti.cn';
 
 const api={
   login: title + '/user/login',//用户登录
