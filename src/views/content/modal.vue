53685399587a347673bcc61b6b457969c5b1e27f
maoyaY <1335679005@qq.com>
2018-09-06 16:52:41 +0800

    弹框首页

--- src/views/content/modal.vue
+++ src/views/content/modal.vue
@@ -15,97 +15,69 @@
           <span class="m-tab-name ">我的</span>
         </li>
       </ul>
-      <div class="m-form-item">
-        <p class="m-form-label">商品名称</p>
-        <div class="m-item-content">
-          <div class=" m-item-row">
-            <el-select v-model="value" class="m-input-l" placeholder="请选择">
-              <el-option
-                v-for="item in options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value">
-              </el-option>
-            </el-select>
-          </div>
-        </div>
+      <!--首页-->
+     <div class="m-index">
+       <el-form :label-position="labelPosition" label-width="100px" :model="formIndex">
+         <div class="m-form-item m-item-modal">
+           <span class="m-item-add">+</span>
+           <p class="m-form-label">浮窗管理</p>
+           <el-form-item label="任务标题">
+             <el-input v-model="formIndex.value" class="m-input-m"></el-input>
+           </el-form-item>
+           <el-form-item label="副标题">
+             <el-input v-model="formIndex.value" class="m-input-m"></el-input>
+           </el-form-item>
+           <el-form-item label="图标" class="m-s">
+             <el-upload
+               class="avatar-uploader"
+               action="https://jsonplaceholder.typicode.com/posts/"
+               :show-file-list="false"
+               :on-success="handleAvatarSuccess"
+               :before-upload="beforeAvatarUpload">
+               <img v-if="imageUrl" :src="imageUrl" class="avatar">
+               <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+             </el-upload>
+           </el-form-item>
+           <el-form-item label="内容">
+             <el-input v-model="formIndex.value" class="m-input-m"></el-input>
+           </el-form-item>
+           <el-form-item label="完成度">
+             <el-input v-model="formIndex.value" class="m-input-m"></el-input>
+           </el-form-item>
+           <el-form-item label="备注">
+             <el-input v-model="formIndex.value" class="m-input-m"></el-input>
+           </el-form-item>
+           <el-form-item label="图片" class="m-f">
+             <el-upload
+               class="avatar-uploader"
+               action="https://jsonplaceholder.typicode.com/posts/"
+               :show-file-list="false"
+               :on-success="handleAvatarSuccess"
+               :before-upload="beforeAvatarUpload">
+               <img v-if="imageUrl" :src="imageUrl" class="avatar">
+               <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+             </el-upload>
+           </el-form-item>
+         </div>
+         <el-form-item label="备注">
+           <textarea v-model="formIndex.value" class="m-textarea" placeholder="请输入内容"></textarea>
+         </el-form-item>
+       </el-form>
+
+     </div>
+      <!--发现-->
+      <div class="m-discovery">
+
+
       </div>
-      <div class="m-form-item">
-        <p class="m-form-label">商家头像</p>
-        <div class="m-item-content">
-          <div class=" m-item-row">
-            <el-upload
-              class="avatar-uploader"
-              action="https://jsonplaceholder.typicode.com/posts/"
-              :show-file-list="false"
-              :on-success="handleAvatarSuccess"
-              :before-upload="beforeAvatarUpload">
-              <img v-if="imageUrl" :src="imageUrl" class="avatar">
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
-          </div>
-        </div>
+      <!--我的-->
+      <div class="m-personal">
+
+
       </div>
-      <div class="m-form-item">
-        <p class="m-form-label">商家描述</p>
-        <div class="m-item-content">
-          <div class=" m-item-row">
-            <textarea v-model="value" class="m-textarea" placeholder="请输入内容"></textarea>
-          </div>
-        </div>
-      </div>
-      <div class="m-form-item  m-item-m">
-        <span class="m-item-add">+</span>
-        <p class="m-form-label">商家图片</p>
-        <div class="m-item-content">
-          <div class=" m-item-row m-s">
-            <el-upload
-              class="avatar-uploader"
-              action="https://jsonplaceholder.typicode.com/posts/"
-              :show-file-list="false"
-              :on-success="handleAvatarSuccess"
-              :before-upload="beforeAvatarUpload">
-              <img v-if="imageUrl" :src="imageUrl" class="avatar">
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
-            <el-select v-model="value"  placeholder="请选择">
-              <el-option
-                v-for="item in options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value">
-              </el-option>
-            </el-select>
-          </div>
-          <p class="m-item-alert">轮播图尺寸大小750*280</p>
-        </div>
-      </div>
-      <div class="m-form-item">
-        <p class="m-form-label">教程视频</p>
-        <div class="m-item-content">
-          <div class=" m-item-row">
-            <el-upload
-              class="avatar-uploader"
-              action="https://jsonplaceholder.typicode.com/posts/"
-              :show-file-list="false"
-              :on-success="handleAvatarSuccess"
-              :before-upload="beforeAvatarUpload">
-              <img v-if="imageUrl" :src="imageUrl" class="avatar">
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
-          </div>
-        </div>
-      </div>
-      <div class="m-form-item">
-        <p class="m-form-label">评论管理</p>
-        <div class="m-item-content">
-          <div class=" m-item-row">
-            <el-input v-model="value" placeholder="请输入内容"></el-input>
-          </div>
-        </div>
-      </div>
-      <div class="m-form-confirm-btn">
-        <span>确定</span>
+      <div class="m-form-confirm-btn ">
+        <span>暂停</span>
+        <span>发布</span>
       </div>
     </div>
   </div>
@@ -134,7 +106,11 @@
           label: '北京烤鸭'
         }],
         value: '',
-        imageUrl:''
+        imageUrl:'',
+        labelPosition:'left',
+        formIndex:{
+          value:''
+        }
       }
     },
     components:{
