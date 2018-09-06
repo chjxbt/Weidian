16466135e537778d28551e62f5fdc6db83360eed
shuaibinliu <13588046135@163.com>
2018-09-06 15:06:24 +0800

    model complain

--- WeiDian/models/model.py
+++ WeiDian/models/model.py
@@ -638,6 +638,15 @@ class MyCenter(BaseModel):
 
 
 
+class Complain(BaseModel):
+    """投诉"""
+    tablename = 'complain'
+    COid = Column(String(64), primary_key=True)
+    COcontent = Column(Text)           # 投诉内容
+    COtype = Column(Integer)           # 投诉类型 {201："客服态度差", 202："商品质量问题", 203："售后方案不合理", 204："商品包装问题"}
+    ORid = Column(String(64))          # 关联订单id
+    USid = Column(String(64))          # 发起人id
+    COcreateTime = Column(String(14))  # 创建时间
 
 
 
