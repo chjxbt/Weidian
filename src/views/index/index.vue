5bd8c280c963906bbb60b55c7fbbcaebf3466ad8
maoyaY <1335679005@qq.com>
2018-09-13 16:37:37 +0800

    首页请求参数修改

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -115,22 +115,6 @@
 
         </div>
       </div>
-      <!--<share v-if="show_fixed"  @fixedClick="fixedClick" @share="share"></share>-->
-
-      <!--<div class="m-modal m-share" v-if="show_fixed">-->
-        <!--<div class="m-modal-state">-->
-          <!--<div class="m-modal-head">-->
-            <!--<span class="m-close" @click="closeModal('show_fixed')"> x </span>-->
-          <!--</div>-->
-          <!--<div class="m-modal-content">-->
-            <!--<h3>还差一步就可以转发内容啦</h3>-->
-           <!--<div>-->
-             <!--<img src="" class="m-share-img" alt="">-->
-             <!--<p>关注公众号获取</p>-->
-           <!--</div>-->
-          <!--</div>-->
-        <!--</div>-->
-      <!--</div>-->
       <attention v-if="show_fixed" @closeModal="closeModal('show_fixed')"></attention>
       <img :src="'/static/images/course/course-'+ course + '.png'" v-if="show_course" class="m-course-img" alt="" @click.stop="courseClick">
       <img src="/static/images/fen.png" v-if="show_fen" class="m-course-img" alt="" @click.stop="fenClick">
@@ -352,7 +336,7 @@
                 lasting:true,
                 start:start || 0,
                 count:count || this.count,
-                tnid:'shangxin'
+                tnid:tnid
               }}).then(res => {
               if(res.data.status == 200){
                 this.isScroll = true;
