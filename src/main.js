4c38547b2b6d56798cdb4953402def12e7531d1f
Liuxy <liuxy0551@qq.com>
2018-09-19 10:29:58 +0800

    替换token

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ FastClick.attach(document.body);
 import promise from 'es6-promise';//解决axios在ie9下不生效的方法
 promise.polyfill();
 
-let token = "eyJhbGciOiJIUzI1NiIsImV4cCI6MTUzNzMwNTM5OCwiaWF0IjoxNTM3MjMzMzk4fQ.eyJtb2RlbCI6IlVzZXIiLCJpZCI6Impma3NhZGpmLWZkYXNsa2pmLTMyMTMtMzEyMzEiLCJ0aW1lIjoiMjAxOC0wOS0xOCAwOToxNjozOCJ9.oxO-z6CteORoYDKa0wkfo3ygtNXL_oslyy_8gYQpXVU";
+let token = "eyJhbGciOiJIUzI1NiIsImV4cCI6MTUzNzM5NjEzNiwiaWF0IjoxNTM3MzI0MTM2fQ.eyJtb2RlbCI6IlVzZXIiLCJpZCI6Impma3NhZGpmLWZkYXNsa2pmLTMyMTMtMzEyMzEiLCJ0aW1lIjoiMjAxOC0wOS0xOSAxMDoyODo1NiJ9.2dLjodhe0nEjdZLUzffS2aObv5KweDtw_MGpsETA4hM";
 localStorage.setItem('token', token);
 
 
