f5d265e390c60387d4ed5910917518d3e05addcd
liuxy0551 <liuxy0551@qq.com>
2018-09-27 22:38:27 +0800

    选择地址的逻辑优化

--- src/views/personal/setUp/editAddress.vue
+++ src/views/personal/setUp/editAddress.vue
@@ -16,11 +16,11 @@
           <span class="m-editAddress-row-name">收货地址</span>
           <div>
             <div class="m-editAddress-input-box">
-              <input type="text" v-model="address.province" @click="pickerSave(true,'','province')" class="m-editAddress-input m-s" placeholder="选择省">
+              <input type="text" v-model="address.province" @click="pickerSave(true,'','province')" class="m-editAddress-input m-s" placeholder="选择省" readonly>
               <span>-</span>
-              <input type="text" v-model="address.city" @click="pickerSave(true,'','city')" class="m-editAddress-input m-s" placeholder="选择市">
+              <input type="text" v-model="address.city" @click="pickerSave(true,'','city')" class="m-editAddress-input m-s" placeholder="选择市" readonly>
               <span>-</span>
-              <input type="text" v-model="address.area" @click="pickerSave(true,'','area')" class="m-editAddress-input m-s" placeholder="选择区(县)">
+              <input type="text" v-model="address.area" @click="pickerSave(true,'','area')" class="m-editAddress-input m-s" placeholder="选择区(县)" readonly>
             </div>
             <div class="m-editAddress-input-box">
               <input type="text" v-model="form.UAtext" class="m-editAddress-input" placeholder="输入详情街道地址">
@@ -53,150 +53,175 @@
   import common from '../../../common/js/common';
   Vue.component(Picker.name, Picker);
     export default {
-        data() {
-            return {
-                radio_select:true,
-               form:{
-                 UAdefault: false,
-                 UAname:'',
-                 UAphone:'',
-                 UAtext:'',
-                 UAid:''
-               },
-              show_picker:false,
-              address_id:{
-                provinceid:'',
-                cityid:'',
-                areaid:''
-              },
-              address:{
-                province:'',
-                city:'',
-                area:''
-              },
-              address_list:{
-                  province:[],
-                city:[],
-                area:[]
-              },
-              slots: [
-                {
-                  flex: 1,
-                  values: ['浙江', '上海', '重庆', '四川', '安徽'],
-                  className: 'slot1',
-                  textAlign: 'center'
+      data() {
+        return {
+          radio_select:true,
+          form:{
+            UAdefault: false,
+            UAname:'',
+            UAphone:'',
+            UAtext:'',
+            UAid:''
+          },
+          show_picker:false,
+          address_id:{
+            provinceid:'',
+            cityid:'',
+            areaid:''
+          },
+          address:{
+            province:'',
+            city:'',
+            area:''
+          },
+          address_list:{
+              province:[],
+            city:[],
+            area:[]
+          },
+          slots: [
+            {
+              flex: 1,
+              values: ['浙江', '上海', '重庆', '四川', '安徽'],
+              className: 'slot1',
+              textAlign: 'center'
+            }
+          ],
+          params:'province'
+        }
+      },
+      components: {
+        picker
+      },
+      methods: {
+        getSlot(id){
+          switch (id){
+            case 'province':
+              // console.log("province", id);
+              axios.get(api.get_province).then(res => {
+                if(res.data.status == 200){
+                  this.address_list[id] = res.data.data;
+                  this.slots[0].values = this.dealArr(res.data.data);
                 }
-              ],
-              params:'province'
-            }
-        },
-        components: {
-          picker
-        },
-        methods: {
-          getSlot(id){
-            switch (id){
-              case 'province':
-                axios.get(api.get_province).then(res => {
-                    if(res.data.status == 200){
-                      this.address_list[id] = res.data.data;
-                      this.slots[0].values = this.dealArr(res.data.data);
-                    }
-                });
-                break;
-              case 'city':
-                axios.get(api.get_city,{
-                  params:{
-                    provinceid:this.address_id.provinceid
-                  }
-                }).then(res => {
-                  if(res.data.status == 200){
-                    this.address_list[id] = res.data.data;
-                    this.slots[0].values = this.dealArr(res.data.data);
-                  }
-                });
-                break;
-              case 'area':
-                axios.get(api.get_area,{
-                  params:{
-                    cityid:this.address_id.cityid
-                  }
-                }).then(res => {
-                  if(res.data.status == 200){
-                    this.address_list[id] = res.data.data;
-                    this.slots[0].values = this.dealArr(res.data.data);
-                  }
-                });
-                break;
-            }
+              });
+              break;
+            case 'city':
+              // console.log("city", id);
+              axios.get(api.get_city,{
+                params:{
+                  provinceid:this.address_id.provinceid
+                }
+              }).then(res => {
+                if(res.data.status == 200){
+                  this.address_list[id] = res.data.data;
+                  this.slots[0].values = this.dealArr(res.data.data);
+                }
+              });
+              break;
+            case 'area':
+              // console.log("area", id);
+              axios.get(api.get_area,{
+                params:{
+                  cityid:this.address_id.cityid
+                }
+              }).then(res => {
+                if(res.data.status == 200){
+                  this.address_list[id] = res.data.data;
+                  this.slots[0].values = this.dealArr(res.data.data);
+                }
+              });
+              break;
+          }
 
-          },
-          radioChange(){
-            this.form.UAdefault = !this.form.UAdefault;
-          },
-          pickerSave(v,target,id){
+        },
+        radioChange(){
+          this.form.UAdefault = !this.form.UAdefault;
+        },
+        pickerSave(v,target,id){
+
+          // 取消按钮
+          if(!v) {
             this.show_picker = v;
-            if(target){
-              this.address[id] = target;
-              for(let i = 0;i<this.address_list[id].length;i++){
-                if(this.address_list[id][i].name == target){
-                  this.address_id[id + 'id'] = this.address_list[id][i][id +'id'];
-                }
-              }
-            }else if(target == '' && id){
-              this.params = id;
-             this.getSlot(id);
+          }
+
+          if(id == 'province') {
+            this.show_picker = v;
+          }else if(id == 'city') {
+            if(this.address_id.provinceid) {
+              this.show_picker = v;
+            }else {
+              Toast('请依次选择省-市-区/县');
             }
-          },
-          saveClick(){
-            this.form.UAdefault = Boolean(this.form.UAdefault);
-            this.form.areaid = this.address_id.areaid;
-            if(this.form.UAid){
-              axios.post(api.update_address +'?token='+localStorage.getItem('token') + '&uaid=' + this.form.UAid,this.form).then(res => {
-                  if(res.data.status == 200 ){
-                    Toast({ message: '修改成功', className: 'm-toast-success' });
-                    this.$router.push('/receiverAddress');
-                  }else{
-                    Toast({ message: res.data.message, className: 'm-toast-warning' });
-                  }
-              })
-            }else{
-              axios.post(api.add_address +'?token='+localStorage.getItem('token'),this.form).then(res => {
+          }else if(id == 'area') {
+            if(this.address_id.provinceid  && this.address_id.cityid) {
+              this.show_picker = v;
+            }else {
+              Toast('请依次选择省-市-区/县');
+            }
+          }
+
+          if(target){
+            this.address[id] = target;
+            for(let i = 0;i<this.address_list[id].length;i++){
+              if(this.address_list[id][i].name == target){
+                this.address_id[id + 'id'] = this.address_list[id][i][id +'id'];
+              }
+            }
+          }else if(target == '' && id && this.show_picker){
+            this.params = id;
+            this.getSlot(id);
+          }
+
+        },
+        saveClick(){
+          this.form.UAdefault = Boolean(this.form.UAdefault);
+          this.form.areaid = this.address_id.areaid;
+          if(this.form.UAid){
+            axios.post(api.update_address +'?token='+localStorage.getItem('token') + '&uaid=' + this.form.UAid,this.form).then(res => {
                 if(res.data.status == 200 ){
-                  Toast({ message: '添加成功', className: 'm-toast-success' });
+                  Toast({ message: '修改成功', className: 'm-toast-success' });
                   this.$router.push('/receiverAddress');
                 }else{
                   Toast({ message: res.data.message, className: 'm-toast-warning' });
                 }
-              })
-            }
-          },
-          delClick(){
-            // MessageBox.confirm('确定执行此操作?').then(action => {
-            //   if(action == 'confirm'){
-                axios.post(api.del_address +'?token='+ localStorage.getItem('token'),{
-                  UAid:this.form.UAid
-                }).then(res => {
-                  if(res.data.status ==200){
-                    Toast({ message: '删除成功', className: 'm-toast-success' });
-                    this.$router.push('/receiverAddress');
-                  }else{
-                    Toast({ message: res.data.message, className: 'm-toast-warning' });
-                  }
-                })
-            //   }else{
-            //   }
-            // })
-
-          },
-          dealArr(list){
-            let arr = [];
-            for(let i=0;i<list.length;i++){
-              arr.push(list[i].name);
-            }
-            return arr;
+            })
+          }else{
+            axios.post(api.add_address +'?token='+localStorage.getItem('token'),this.form).then(res => {
+              if(res.data.status == 200 ){
+                Toast({ message: '添加成功', className: 'm-toast-success' });
+                this.$router.push('/receiverAddress');
+              }else{
+                Toast({ message: res.data.message, className: 'm-toast-warning' });
+              }
+            })
           }
         },
+        delClick(){
+        // MessageBox.confirm('确定执行此操作?').then(action => {
+        //   if(action == 'confirm'){
+            axios.post(api.del_address +'?token='+ localStorage.getItem('token'),{
+              UAid:this.form.UAid
+            }).then(res => {
+              if(res.data.status ==200){
+                Toast({ message: '删除成功', className: 'm-toast-success' });
+                this.$router.push('/receiverAddress');
+              }else{
+                Toast({ message: res.data.message, className: 'm-toast-warning' });
+              }
+            })
+        //   }else{
+        //   }
+        // })
+
+        },
+        dealArr(list){
+          let arr = [];
+          for(let i=0;i<list.length;i++){
+            arr.push(list[i].name);
+          }
+          return arr;
+        }
+      },
       mounted(){
           if(this.$route.query){
             this.form.UAdefault = this.$route.query.UAdefault;
@@ -211,12 +236,12 @@
             this.address_id.cityid = this.$route.query.cityid;
             this.address_id.areaid = this.$route.query.areaid;
           }
-          this.getSlot('province');
+          // this.getSlot('province');
         common.changeTitle('编辑地址');
       },
-        created() {
+      created() {
 
-        }
+      }
     }
 </script>
 <style lang="less" rel="stylesheet/less" scoped>
@@ -251,7 +276,7 @@
           border: none;
           height: 85px;
           line-height: 85px;
-          font-size: 30px;
+          font-size: 28px;
           &.m-s{
             width: 28%;
             &::-webkit-input-placeholder {
