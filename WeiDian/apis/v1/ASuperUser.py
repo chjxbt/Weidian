e88f0f97f342c722e4d5bbfe16b0b930c2780006
wiilz <rayleignze@gmail.com>
2018-10-10 23:07:04 +0800

    管理员模块相关功能

--- WeiDian/apis/v1/ASuperUser.py
+++ WeiDian/apis/v1/ASuperUser.py
@@ -12,12 +12,21 @@ class ASuperUser(Resource):
     def __init__(self):
         self.csuperuser = CSuperUser()
 
+    def get(self, super):
+        print super
+        apis = {
+            'get_all_suser': "self.csuperuser.get_all_admin_by_filter()"
+        }
+        res = eval(apis[super])
+        return jsonify(res)
+
     def post(self, super):
         print super
         apis = {
             'login': 'self.csuperuser.login()',
             'update_info': 'self.csuperuser.update_suerinfo()',
-            'add_admin': "self.csuperuser.add_admin_by_superadmin()"
+            'add_admin': "self.csuperuser.add_admin_by_superadmin()",
+            'update_other_admin': "self.csuperuser.superadmin_to_update_others()"
         }
         res = eval(apis[super])
         return jsonify(res)
