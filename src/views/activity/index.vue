192ff92cc5127ea3e094a9c6bcc9fe5c57fc0e47
Liuxy <liuxy0551@qq.com>
2018-10-15 14:43:55 +0800

    活动中心

--- src/views/activity/index.vue
+++ src/views/activity/index.vue
@@ -2,18 +2,17 @@
   <div class="m-weidian">
     <page-title :title="name" ></page-title>
     <div class="m-weidian-content">
-      <p class="m-form-label" style="margin-bottom: 0.2rem">任务等级奖励 </p>
-
+      <!--<p class="m-form-label" style="margin-bottom: 0.2rem">任务等级奖励 </p>
       <p class="m-form-label" style="margin-bottom: 0.1rem">任务等级奖励方式管理</p>
-      <!--<div class="content-table">
-        <el-table :data="hiddenList" border style="width: 100%" v-loading="hiddenLoading">
-          <el-table-column prop="name" label="板块" width="260"></el-table-column>
-          <el-table-column prop="content" label="隐藏内容" width="500"></el-table-column>
-          <el-table-column prop="show" label="是否显示" width="300">
+      <div class="content-table">
+        <el-table :data="levelList" border style="width: 100%" v-loading="levelLoading">
+          <el-table-column prop="name" label="任务等级" width="260"></el-table-column>
+          <el-table-column prop="content" label="奖励方式" width="500"></el-table-column>
+          <el-table-column prop="num" label="管理">
             <template slot-scope="scope">
-              <div @click="rowClick(scope.$index)">
-                <el-switch v-model="scope.row.show" active-text="展示" inactive-text="隐藏" @change="hiddenRow"></el-switch>
-              </div>
+              <el-button type="text" size="small" @click="">编辑</el-button>
+              <el-button type="text" size="small">|</el-button>
+              <el-button type="text" size="small" @click="">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -25,10 +24,13 @@
       </div>
       <div class="content-table">
         <el-table :data="discountsList" border style="width: 100%" v-loading="discountsLoading">
+          <el-table-column prop="type" label="优惠名称"></el-table-column>
           <el-table-column prop="type" label="优惠类型"></el-table-column>
           <el-table-column prop="status" label="状态"></el-table-column>
           <el-table-column prop="discounts" label="优惠"></el-table-column>
+          <el-table-column prop="discounts" label="使用时间"></el-table-column>
           <el-table-column prop="num" label="已领取"></el-table-column>
+          <el-table-column prop="num" label="集合"></el-table-column>
           <el-table-column prop="num" label="管理">
             <template slot-scope="scope">
               <el-button type="text" size="small" @click="">编辑</el-button>
@@ -42,41 +44,62 @@
 
       <p class="m-form-label" style="margin-bottom: 0.1rem; font-size: 0.14rem">优惠编辑</p>
       <div class="input-box">
+        <div class="box-text">优惠名称</div>
+        <div class="box-right">
+          <el-input v-model="discountsName" style="width: 2rem" size="small" placeholder="请输入优惠门槛"></el-input>
+        </div>
+        <div class="box-text">元</div>
+      </div>
+      <div class="input-box">
         <div class="box-text">优惠类型</div>
         <div class="box-right">
-          <el-select v-model="type" placeholder="请选择" style="width: 2rem" size="mini">
+          <el-select v-model="type" placeholder="请选择优惠类型" style="width: 2rem" size="mini">
             <el-option v-for="item in typeList" :key="item.value" :label="item.label" :value="item.value">
             </el-option>
           </el-select>
         </div>
       </div>
-
+      <div class="input-box">
+        <div class="box-text">使用渠道</div>
+        <div class="box-right">
+          <el-select v-model="useMethod" placeholder="请选择优惠类型" style="width: 2rem" size="mini">
+            <el-option v-for="item in useMethodList" :key="item.value" :label="item.label" :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
+      </div>
+      <div class="input-box">
+        <div class="box-text">可否转赠</div>
+        <div class="box-right">
+          <el-select v-model="giveToOne" placeholder="请选择优惠类型" style="width: 2rem" size="mini">
+            <el-option v-for="item in giveToOneList" :key="item.value" :label="item.label" :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
+      </div>
       <div class="input-box" v-if="type != 2">
         <div class="box-text">优惠金额</div>
         <div class="box-right">
-          <el-input v-model="amount" style="width: 2rem" size="small" placeholder="请输入"></el-input>
+          <el-input v-model="amount" style="width: 2rem" size="small" placeholder="请输入优惠金额"></el-input>
         </div>
         <div class="box-text">元</div>
       </div>
-
       <div class="input-box" v-if="type == 2">
         <div class="box-text">佣金加成</div>
         <div class="box-right">
-          <el-select v-model="bonus" placeholder="请选择" style="width: 2rem" size="mini">
+          <el-select v-model="bonus" placeholder="请选择佣金加成比率" style="width: 2rem" size="mini">
             <el-option v-for="item in bonusList" :key="item.value" :label="item.label" :value="item.value">
             </el-option>
           </el-select>
         </div>
       </div>
-
       <div class="input-box" v-if="type != 3">
         <div class="box-text">优惠门槛</div>
         <div class="box-right">
-          <el-input v-model="threshold" style="width: 2rem" size="small" placeholder="满减"></el-input>
+          <el-input v-model="threshold" style="width: 2rem" size="small" placeholder="请输入优惠门槛"></el-input>
         </div>
         <div class="box-text">元</div>
       </div>
-
       <div class="input-box">
         <div class="box-text">使用时间</div>
         <div class="box-right">
@@ -96,39 +119,74 @@
 
 <script>
   import pageTitle from '../../components/common/title';
+  import axios from 'axios';
+  import api from '../../api/api';
+
   export default {
     data(){
       return{
         name:'活动中心',
-        discountsList: [  // 平台优惠list
+        discountsList: [          // 平台优惠list
           { type: "满减",status: "领取中", discounts: "满100-20元", time: "2018/08/05  至  2018/09/05", num: "100" },
           { type: "佣金加成",status: "暂停", discounts: "佣金+20%", time: "2018/08/05  至  2018/09/05", num: "200" },
           { type: "无门槛",status: "已结束", discounts: "减50元", time: "2018/08/05  至  2018/09/05", num: "300" }
         ],
         discountsLoading: false,  // 平台优惠list加载中
-        type: "",         // 优惠类型
-        typeList: [       // 优惠类型list
+        discountsName: "",        // 优惠名称
+        type: "",                 // 优惠类型
+        typeList: [               // 优惠类型list
           { value: "1", label: "满减" },
           { value: "2", label: "佣金加成" },
           { value: "3", label: "无门槛" }
         ],
-        amount: "",       // 优惠金额
-        bonus: "",        // 佣金加成
-        bonusList: [      // 佣金加成list
+        useMethod: "",            // 使用渠道
+        useMethodList: [          // 使用渠道list
+          { value: "1", label: "满减" },
+          { value: "2", label: "佣金加成" },
+          { value: "3", label: "无门槛" }
+        ],
+        giveToOne: "",            // 可否转赠
+        giveToOneList: [          // 可否转赠list
+          { value: "1", label: "可转增" },
+          { value: "2", label: "不可转增" }
+        ],
+        // levelList: [],            // 任务等级奖励管理
+        // levelLoading: false,      // 任务等级奖励管理
+        amount: "",               // 优惠金额
+        bonus: "",                // 佣金加成
+        bonusList: [              // 佣金加成list
           { value: "1", label: "10%" },
           { value: "2", label: "20%" },
           { value: "3", label: "30%" },
           { value: "4", label: "40%" },
           { value: "5", label: "50%" }
         ],
-        threshold: "",    // 优惠门槛
-        time: "",         // 使用时间
-
+        threshold: "",            // 优惠门槛
+        time: "",                 // 使用时间
       }
     },
     components:{ pageTitle },
     methods:{
-
+      // 获取所有任务等级
+      /*getAllTaskLevel() {
+        this.levelLoading = true;
+        axios.get(api.get_all_task_level + '?token=' + localStorage.getItem('token')).then(res => {
+          if(res.data.status == 200){
+            this.levelList = res.data.data;
+            this.levelLoading = false;
+            for(let i = 0; i < this.levelList.length; i ++) {
+              this.levelList[i].talevel = '等级' + this.levelList[i].talevel;
+              this.levelList[i].disabled = true;
+            }
+            console.log(this.levelList)
+          }else{
+            this.$message({ type: 'error', message: res.data.message, duration: 1500 });
+          }
+        });
+      },*/
+    },
+    mounted() {
+      // this.getAllTaskLevel();   // 获取所有任务等级
     }
   }
 </script>
@@ -139,13 +197,14 @@
   .coupons-box {
     display: flex;
     .add-coupons-btn {
+      width: 0.6rem;
       color: #ffffff;
       font-size: 0.12rem;
-      padding: 0.05rem 0.3rem;
-      white-space: normal;
+      padding: 0.05rem 0.2rem;
+      white-space: nowrap;
       background-color: #91aeb5;
       border-radius: 0.1rem;
-      margin-bottom: 0.05rem;
+      margin-bottom: 0.1rem;
     }
   }
 
