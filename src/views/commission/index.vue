20e88da030efa21be9d961c5d6b6280e67b598cb
Liuxy <liuxy0551@qq.com>
2018-10-17 15:06:48 +0800

    佣金管理

--- src/views/commission/index.vue
+++ src/views/commission/index.vue
@@ -2,34 +2,94 @@
   <div class="m-weidian">
     <page-title :title="name" ></page-title>
     <div class="m-weidian-content">
-      <el-form :inline="true" :model="form" class="demo-form-inline">
-        <div class="m-select-box">
-          <el-form-item label="店主ID">
-            <el-input v-model="form.name" class="m-input-s m-green-b" placeholder=""></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" class="m-select-btn" @click="storeSubmit">查询</el-button>
-          </el-form-item>
+      <w-tab :list="tab_list"  @wTabClick="wTabClick"></w-tab>
+
+      <p class="m-form-label" style="margin: 0.3rem 0 0.1rem 0">人员条件</p>
+      <div class="num-list">
+        <div class="num-box">
+          <p class="m-form-label two-title">人数：</p>
+          <div class="m-item-content">
+            <div class="m-item-row">
+              <el-input v-model="peopleNum" class="m-input-s" size="mini" placeholder="请输入人数条件">
+                <template slot="append">人</template>
+              </el-input>
+            </div>
+          </div>
         </div>
-      </el-form>
-      <div class="m-middle" style="width: 100%;margin-top: 0.1rem;">
-        <el-table :data="user" stripe style="width: 100%">
-          <el-table-column align="center" prop="userName" label="店主ID"></el-table-column>
-          <el-table-column align="center" prop="userId" label="店主等级" ></el-table-column>
-          <el-table-column align="center" prop="userName" label="联系方式"></el-table-column>
-          <el-table-column align="center" prop="group" label="获得总佣金" ></el-table-column>
-          <el-table-column align="center" prop="group" label="销售金额佣金" ></el-table-column>
-          <el-table-column align="center" prop="group" label="邀请开店佣金" ></el-table-column>
-          <el-table-column align="center" prop="userName" label="专粉支出佣金"></el-table-column>
-          <el-table-column align="center" prop="userId" label="团队佣金" ></el-table-column>
-          <el-table-column align="center" prop="userName" label="奖励佣金"></el-table-column>
-          <el-table-column align="center" prop="group" label="账户余额" ></el-table-column>
-          <el-table-column align="center" prop="group" label="待到账金额" ></el-table-column>
-          <el-table-column align="center" prop="group" label="正提现金额" ></el-table-column>
-        </el-table>
+        <div class="num-box">
+          <p class="m-form-label two-title">额度：</p>
+          <div class="m-item-content">
+            <div class="m-item-row">
+              <el-input v-model="limitNum" class="m-input-s" placeholder="请输入额度条件">
+                <template slot="append">元</template>
+              </el-input>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="m-page-box">
-        <Pagination :total="page_data.total_page" @pageChange="pageChange"></Pagination>
+
+      <p class="m-form-label" style="margin: 0.2rem 0 0.1rem 0">三级提点</p>
+      <div class="num-list">
+        <div class="num-box">
+          <p class="m-form-label two-title">每单销售百分比：</p>
+          <div class="m-item-content">
+            <div class="m-item-row">
+              <el-input v-model="salesPercentage" class="m-input-s" size="mini" placeholder="请输入佣金百分比">
+                <template slot="append">%</template>
+              </el-input>
+            </div>
+          </div>
+        </div>
+        <div class="num-box">
+          <p class="m-form-label two-title">邀请普通合伙人大礼包金额：</p>
+          <div class="m-item-content">
+            <div class="m-item-row">
+              <el-input v-model="giftAmount" class="m-input-s" placeholder="请输入大礼包金额">
+                <template slot="append">元</template>
+              </el-input>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="num-list">
+        <div class="num-box">
+          <p class="m-form-label two-title">团队月月奖时间：</p>
+          <div class="m-item-content">
+            <div class="m-item-row">
+              <el-date-picker v-model="monthRewardTime" type="datetimerange" range-separator="至" value-format="yyyy-MM-dd HH:mm:ss"
+                              start-placeholder="开始日期" end-placeholder="结束日期" style="width: 4rem;">
+              </el-date-picker>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="num-list">
+        <div class="num-box">
+          <p class="m-form-label two-title">团队月月奖金额：</p>
+          <div class="m-item-content">
+            <div class="m-item-row">
+              <el-input v-model="rewardAmount" class="m-input-s" size="mini" placeholder="请输入月月奖金额">
+                <template slot="append">元</template>
+              </el-input>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="num-list">
+        <div class="num-box">
+          <p class="m-form-label two-title">活动期间奖项时间：</p>
+          <div class="m-item-content">
+            <div class="m-item-row">
+              <el-date-picker v-model="activityTime" type="datetimerange" range-separator="至" value-format="yyyy-MM-dd HH:mm:ss"
+                              start-placeholder="开始日期" end-placeholder="结束日期" style="width: 4rem;">
+              </el-date-picker>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="m-form-confirm-btn">
+        <span @click="">保 存</span>
       </div>
     </div>
   </div>
@@ -37,46 +97,41 @@
 
 <script>
   import pageTitle from '../../components/common/title';
-  import Pagination from "../../components/common/page";
+  import wTab from '../../components/common/wTab';
+
   export default {
     data(){
       return{
         name:'佣金管理',
-        form:{
-          name:''
-        },
-        page_data:{
-          total_page:1,
-          current_page:1,
-          total_num:0,
-          page_size:10
-        },
-        user:[
-          {
-            group:'',
-            userName:'',
-            userId:'',
-
-          }
-        ]
+        tab_list: [               // 顶部导航的list
+          { active: true, name: "高级合伙人", tnid: "gaoji", url: "" },
+          { active: false, name: "中级合伙人", tnid: "zhongji", url: "" },
+          { active: false, name: "普通合伙人", tnid: "putong", url: "" }
+        ],
+        tnid: "",                 // tab栏的id
+        peopleNum: "",            // 人数条件
+        limitNum: "",             // 额度条件
+        salesPercentage: "",      // 销售百分比
+        giftAmount: "",           // 合伙人大礼包金额
+        monthRewardTime: [],      // 团队月月奖时间
+        rewardAmount: "",         // 团队月月奖金额
+        activityTime: [],         // 活动期间奖项时间
       }
     },
-    components:{
-      pageTitle,
-      Pagination
-    },
+    components:{ pageTitle, wTab },
     methods:{
-      /*分页点击*/
-      pageChange(v){
-        // if(v == this.current_page){
-        //   this.$message({ message: '这已经是第' + v + '页数据了', type: 'warning', duration: 1500 });
-        //   return false;
-        // }
-        // this.current_page = v;
 
-      },
-      storeSubmit(){
+      // 顶部tab栏的点击事件
+      wTabClick(i){
+        let arr = [].concat(this.tab_list);
+        for(let a =0;a<arr.length;a++){
+          arr[a].active = false;
+        }
+        arr[i].active = true;
+        this.tab_list = [].concat(arr);
 
+        this.tnid = this.tab_list[i].tnid;
+        // this.getActivity(0, this.page_size);   // 获取首页活动/推文内容列表
       },
     }
   }
@@ -84,4 +139,15 @@
 
 <style lang="less" rel="stylesheet/less" scoped>
   @import "../../common/css/weidian";
+
+  .num-list {
+    display: flex;
+    .num-box {
+      margin-right: 0.5rem;
+      .two-title {
+        font-size: 0.12rem;
+        margin-top: 0.05rem;
+      }
+    }
+  }
 </style>
